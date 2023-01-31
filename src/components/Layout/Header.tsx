c0f069ebe96374fc950711b342dd218d0aaf6880
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-31 17:37:13 +0400

    chore: add header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,35 +1,61 @@
-import { Flex, Heading, Text } from '@chakra-ui/react'
-import { useAccount } from 'wagmi'
+import getAvatar from '@/utils/avatarUtils'
+import { formatAddress } from '@/utils/formattingUtils'
+import { Button, Flex, Image } from '@chakra-ui/react'
+import { useRouter } from 'next/router';
 
 // header component with wagmi connect button
 export default function Header() {
-    const { address, isConnected } = useAccount()
+    // const { address, isConnected } = useAccount()
+    const address = '0x78d86E031605d2873f38dfa2657Fe92d8aC243f9';
+    const router = useRouter()
 
     return (
-        <header>
+        <Flex
+            as='header'
+            p={4}
+        >
+            <Image
+                onClick={
+                    () => {
+                        router.push({
+                            pathname: '/'
+                        })
+                    }
+                }
+                display={{ base: 'none', lg: 'inherit' }}
+                mr='auto'
+                src='/zeroWallet.svg'
+                alt='ZeroWallet'
+                cursor='pointer'
+            />
             <Flex
                 justifyContent="space-between"
                 alignItems="center"
-                w="100%"
-                h="100%"
-                p="1rem"
-                m="1rem"
+                ml='auto'
+                gap={5}
             >
-                <Flex>
-                    <Heading as="h1" size="lg" color="Highlight" mr="1rem">
-                        Zero Wallet Dashboard
-                    </Heading>
-                </Flex>
-                <Flex
-                    gap={5}
-                    alignItems="center"
-                    justifyContent={isConnected ? 'flex-end' : 'center'}
+                <Button
+                    variant="primary2"
                 >
-                    <Text pr="5rem" color="Highlight">
-                        {isConnected ? address : 'Disconnected'}
-                    </Text>
-                </Flex>
+                    Contact us
+                </Button>
+
+                <Button
+                    gap={2}
+                    background='white'
+                >
+                    <Image
+                        borderRadius='3xl'
+                        src={getAvatar(false, address ?? 'generic')}
+                        boxSize='24px'
+                        alt='avatar'
+                    />
+
+                    {formatAddress(address)}
+                </Button>
+
+
             </Flex>
-        </header>
+        </Flex>
     )
 }
