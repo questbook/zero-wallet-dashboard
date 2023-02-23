91fb38a459be5308c598bb2bd20fe41be52c9485
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-02-23 18:26:39 +0300

    fix: goerli contract address

--- src/contracts/gasTank/index.ts
+++ src/contracts/gasTank/index.ts
@@ -7,7 +7,7 @@ const ADDRESS_BY_CHAIN_ID: {
     [SupportedChainIds.POLYGON]: '0xeb808ba857a080d35554fe5830dc61df1ba53e0c',
     [SupportedChainIds.OPTIMISM]: '0x5Bc7c8cFC6d54c055547Fcd8f6Ee4f2eDcda613e',
     [SupportedChainIds.CELO]: '0xEb394FE49C745D29564B3cC362530031FC741b01',
-    [SupportedChainIds.GOERLI]: '0xE041608922d06a4F26C0d4c27d8bCD01daf1f792',
+    [SupportedChainIds.GOERLI]: '0x7Ad7AD90409977c3A3B5f63D7b4e5530b87c9Cc3',
 }
 
 export { contractAbi as abi, ADDRESS_BY_CHAIN_ID }
